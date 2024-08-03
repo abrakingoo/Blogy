658ebc8e9ec4a3ef84579f7099927b94bbb18442
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 14:46:59 +0300

    fmt: fixed the code to standard go format

--- data/user.go
+++ data/user.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Blog struct {
-	Id string `json:"id"`
-	Title string `json:"title"`
+	Id           string `json:"id"`
+	Title        string `json:"title"`
 	Blog_content string `json:"blog_content"`
 }
 
 type User struct {
-	Name string `json:"name"`
-	Email string `json:"email"`
-	Password string `json:"password"`
-	Create_at time.Time 	`json:"created_at"`
-	Blogs []Blog `json:"blogs"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Create_at time.Time `json:"created_at"`
+	Blogs     []Blog    `json:"blogs"`
 }
