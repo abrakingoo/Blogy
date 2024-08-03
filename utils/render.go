60dd873c3b1c7b2fca2a3cd9eb1a418823135b09
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 14:49:04 +0300

    fmt: fixed the code to standard go format

--- utils/render.go
+++ utils/render.go
@@ -5,16 +5,15 @@ import (
 	"text/template"
 )
 
-
 var templates *template.Template
 
 func init() {
-    templates = template.Must(template.ParseGlob("templates/*.html"))
+	templates = template.Must(template.ParseGlob("templates/*.html"))
 }
 
 func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-    err := templates.ExecuteTemplate(w, tmpl, data)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	err := templates.ExecuteTemplate(w, tmpl, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
