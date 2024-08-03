da579b6ee9b05b1917ecbe3ea7eeae8112f1477d
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 12:39:09 +0300

    feat: created a user and blog structs to represent the data to work with

---
+++ data/user.go
@@ -0,0 +1,19 @@
+package data
+
+import (
+	"time"
+)
+
+type Blog struct {
+	Id string `json:"id"`
+	Title string `json:"title"`
+	Blog_content string `json:"blog_content"`
+}
+
+type User struct {
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+	Create_at time.Time 	`json:"created_at"`
+	Blogs []Blog `json:"blogs"`
+}
