0ec343706765709290e30cb86ad13fff973dd028
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 16:46:06 +0300

    updated the executed data struct

--- handlers/login.go
+++ handlers/login.go
@@ -6,10 +6,14 @@ import (
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+
 	data := struct {
 		Title string
+		VerifiedUser bool
 	}{
 		Title: "login",
+		VerifiedUser: false,
 	}
+
 	utils.RenderTemplate(w, "login.html", data)
 }
