9eef2e4144fe1c0d1371e36f52c0fe863a0e56b0
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 16:45:43 +0300

    updated the executed data struct

--- handlers/home.go
+++ handlers/home.go
@@ -8,8 +8,10 @@ import (
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	data := struct {
 		Title string
+		VerifiedUser bool
 	}{
 		Title: "Home",
+		VerifiedUser: false,
 	}
 	utils.RenderTemplate(w, "index.html", data)
 }
