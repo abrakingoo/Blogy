18a76f4c248eef7824a478f898fad3147f7da2ee
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 16:46:32 +0300

    updated the executed data struct

--- handlers/signup.go
+++ handlers/signup.go
@@ -8,8 +8,10 @@ import (
 func SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	data := struct {
 		Title string
+		VerifiedUser bool
 	}{
 		Title: "signup",
+		VerifiedUser: false,
 	}
 	utils.RenderTemplate(w, "sign.html", data)
 }
