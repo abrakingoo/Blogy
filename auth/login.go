ac68c3442f6f20115e00917d28493421849b5e1b
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 14:45:49 +0300

    refactor: added error handling system'

--- auth/login.go
+++ auth/login.go
@@ -1,7 +1,55 @@
 package auth
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+
+	"practice/data"
+	"practice/utils"
+)
+
+var AuthenticatedUser *data.User
 
 func AuthLogin(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Authenticating.."))
+	users, err := utils.ReadDataBase()
+	if err != nil {
+		data := struct {
+			Message  string
+			Redirect string
+			Inst     string
+		}{
+			Message:  err.Error(),
+			Redirect: "/signup",
+			Inst:     "SignUp",
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+		tpl, _ := template.ParseFiles("templates/errors.html")
+		tpl.Execute(w, data)
+		return
+	}
+
+	err = r.ParseForm()
+	if err != nil {
+		http.Error(w, "error parsing user data", http.StatusInternalServerError)
+		return
+	}
+
+	userEmail := r.FormValue("email")
+	password := r.FormValue("password")
+
+	for _, user := range users {
+		if user.Email == userEmail {
+			if user.Password == password {
+				AuthenticatedUser = &user
+				http.Redirect(w, r, "/profile", http.StatusSeeOther)
+				return
+			} else {
+				http.Error(w, "invalid username/password", http.StatusUnauthorized)
+				return
+			}
+		}
+	}
+
+	http.Error(w, "user not found", http.StatusNotFound)
 }
