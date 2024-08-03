01dec8eb1fe2cf7ea7fb803252dd8c4f2dfdd13a
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 12:19:18 +0300

    feat: added the handling of login and loginauth routes

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"practice/auth"
 	"practice/handlers"
 )
 
 func main(){
 
 	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/loginAuth", auth.AuthLogin)
 	log.Println("http server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
