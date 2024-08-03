bb5c3a614b59cba061d8249e4768978af11090ed
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 12:25:41 +0300

    refactor: moved the RenderTemplate functionality to the utils package to make it accessible to all other handlers in the application

---
+++ utils/render.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"net/http"
+	"text/template"
+)
+
+
+var templates *template.Template
+
+func init() {
+    templates = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+    err := templates.ExecuteTemplate(w, tmpl, data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
