4249b1483cf8f537a0424a86516331b559f90fd7
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 11:51:27 +0300

    refactor: opted to pass the page values using templates to add more dynamics to the page

--- handlers/home.go
+++ handlers/home.go
@@ -1,7 +1,28 @@
 package handlers
 
-import "net/http"
+import (
+    "net/http"
+    "text/template"
+)
+
+var templates *template.Template
+
+func init() {
+    templates = template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+    err := templates.ExecuteTemplate(w, tmpl, data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+}
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "templates/index.html")
+    data := struct {
+        Title string
+    }{
+        Title: "Home",
+    }
+    renderTemplate(w, "index.html", data)
 }
