6ebab25a7d39453a747daa1941b31feeb7b4686e
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 16:51:23 +0300

    feat: customizes every users profile page with the user data

---
+++ handlers/profile.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"net/http"
+
+	"practice/auth"
+	"practice/data"
+	"practice/utils"
+)
+
+func ProfileHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the user is authenticated
+	if auth.AuthenticatedUser == nil {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	cookie, err := auth.Store.Get(r, "userSession")
+	if err != nil {
+		http.Error(w, "error getting session: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	email, ok := cookie.Values["email"].(string)
+
+	if !ok {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if auth.AuthenticatedUser.Email != email {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// Prepare the data for rendering the template
+	data := struct {
+		Title        string
+		VerifiedUser bool
+		Name         string
+		Blogs	[]data.Blog
+	}{
+		Title:        "Profile",
+		VerifiedUser: true, // Adjust as needed based on user verification
+		Name:         auth.AuthenticatedUser.Name,
+		Blogs: auth.AuthenticatedUser.Blogs,
+	}
+
+	// Render the profile template
+	utils.RenderTemplate(w, "profile.html", data)
+}
