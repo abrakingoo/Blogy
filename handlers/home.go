b15d0a28d942820e44a019fb6d4548cc46514d1c
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 21:07:44 +0300

    refactor: added a conditional rendering of the home page based on the user being signed in or not

--- handlers/home.go
+++ handlers/home.go
@@ -2,16 +2,55 @@ package handlers
 
 import (
 	"net/http"
+	"practice/auth"
 	"practice/utils"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	data := struct {
-		Title string
-		VerifiedUser bool
-	}{
-		Title: "Home",
-		VerifiedUser: false,
+	// Retrieve the user session
+	cookie, err := auth.Store.Get(r, "userSession")
+	if err != nil {
+		http.Error(w, "Error getting session: "+err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	// Get email from session
+	email, ok := cookie.Values["email"].(string)
+	if !ok || email == "" {
+		// Redirect to the public home page
+		data := struct {
+			Title        string
+			VerifiedUser bool
+			UserName     string
+		}{
+			Title:        "Home",
+			VerifiedUser: false, // Indicates the user is not signed in
+			UserName:     "",
+		}
+		utils.RenderTemplate(w, "index.html", data)
+		return
+	}
+
+	// If the session is valid, set user details
+	var (
+		signed   bool
+		userName string
+	)
+	
+	if auth.AuthenticatedUser != nil {
+		signed = true
+		userName = auth.AuthenticatedUser.Name
+	}
+
+	data := struct {
+		Title        string
+		VerifiedUser bool
+		UserName     string
+	}{
+		Title:        "Home",
+		VerifiedUser: signed,
+		UserName:     userName,
+	}
+
 	utils.RenderTemplate(w, "index.html", data)
 }
