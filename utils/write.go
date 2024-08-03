1bc32992078e07b05e93c24cdc7dfeb5b5c60a32
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 14:55:04 +0300

    feat: this is a function that is responsible in adding new users to the datatbase after being verified

---
+++ utils/write.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"encoding/json"
+	"os"
+	"practice/data"
+)
+
+func WriteToDataBase(users []data.User, newUser data.User) (bool, error) {
+	filepath := "data/storage.json"
+
+	if len(users) == 0 {
+		users = append(users, newUser)
+
+		marshaledUsers, err := json.MarshalIndent(users, "", " ")
+
+		if err != nil {
+			return false, err
+		}
+
+		err = os.WriteFile(filepath, marshaledUsers, 0644)
+
+		if err != nil {
+			return false, err
+		}
+
+		return true, nil
+	}
+
+	return true, nil
+}
