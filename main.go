1aaf74d290b9aa52798b969b3f2de2153ed15b64
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 16:47:29 +0300

    refactor: added more routes

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func main() {
 	http.HandleFunc("/loginAuth", auth.AuthLogin)
 	http.HandleFunc("/signup", handlers.SignUpHandler)
 	http.HandleFunc("/authSignUp", auth.AuthSignup)
+	http.HandleFunc("/profile", handlers.ProfileHandler)
+	http.HandleFunc("/logout", handlers.LogOutHandler)
 	log.Println("http server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
