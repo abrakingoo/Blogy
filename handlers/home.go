8736d4273d803624acb1370d7e8548d15adf3352
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 12:17:59 +0300

    Refactor: moved the RenderTemplate functionality to make it accessible to othe handlers and keep the code base dry

--- handlers/home.go
+++ handlers/home.go
@@ -1,22 +1,9 @@
 package handlers
 
 import (
-    "net/http"
-    "text/template"
+	"net/http"
+	"practice/utils"
 )
-
-var templates *template.Template
-
-func init() {
-    templates = template.Must(template.ParseGlob("templates/*.html"))
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-    err := templates.ExecuteTemplate(w, tmpl, data)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-}
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
     data := struct {
@@ -24,5 +11,5 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
     }{
         Title: "Home",
     }
-    renderTemplate(w, "index.html", data)
+	utils.RenderTemplate(w, "index.html", data)
 }
