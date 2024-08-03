2d6d85044fcfc81ccfe4c45922e0de9ed309d835
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 11:21:27 +0300

    feat: added a request handler function that handles the get request to the home route

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"practice/handlers"
 )
 
 func main(){
+
+	http.HandleFunc("/", handlers.HomeHandler)
 	log.Println("http server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
