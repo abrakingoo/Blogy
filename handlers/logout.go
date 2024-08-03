2e900ef1b13a981447193f577462291a9c2c00ba
abrakingoo <maikoabraham68@gmail.com>
2024-08-03 21:08:55 +0300

    refactor: added more values to the data element including the user verification status

--- handlers/logout.go
+++ handlers/logout.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"practice/auth"
+	"practice/utils"
 )
 
 func LogOutHandler(w http.ResponseWriter, r *http.Request) {
@@ -23,7 +24,17 @@ func LogOutHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Redirect to login page
-	http.Redirect(w, r, "/login", http.StatusSeeOther)
+
+		data := struct {
+		Title        string
+		VerifiedUser bool
+		UserName     string
+	}{
+		Title:        "Home",
+		VerifiedUser: false,
+		UserName:     "",
+	}
+
+	utils.RenderTemplate(w, "index.html", data)
 }
 
