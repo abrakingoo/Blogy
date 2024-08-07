b1fd7a5fbddd5bef71561c6f6ae6946d795a29b8
abrakingoo <maikoabraham68@gmail.com>
2024-08-07 09:15:58 +0300

    feat: added the functionality where the home handler makes an api call and populates the home page with the articles on launch

--- handlers/home.go
+++ handlers/home.go
@@ -1,20 +1,53 @@
 package handlers
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 	"practice/auth"
+	"practice/data"
 	"practice/utils"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	
+	var articles data.Article
+
 	// Retrieve the user session
 	cookie, err := auth.Store.Get(r, "userSession")
 	if err != nil {
 		http.Error(w, "Error getting session: "+err.Error(), http.StatusInternalServerError)
 		return
 	}
+	url := "https://newsapi.org/v2/top-headlines?country=us&apiKey=d6b4d534b08c4d4aa43339cc6943e773"
 
-	// Get email from session
+	res, err := http.Get(url)
+
+	if err != nil {
+		articles = data.Article{}
+		http.Error(w, "Error Fetching Data: "+err.Error(), http.StatusInternalServerError)
+	}
+
+	defer res.Body.Close()
+
+	resBody, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		http.Error(w, "Error Reading Response Data: "+err.Error(), http.StatusInternalServerError)
+	}
+
+	
+	// fmt.Println(string(resBody))
+	
+	err = json.Unmarshal(resBody, &articles)
+	
+	if err != nil {
+		http.Error(w, "Error Unmarshalling Response Data: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	
+		// Get email from session
 	email, ok := cookie.Values["email"].(string)
 	if !ok || email == "" {
 		// Redirect to the public home page
@@ -22,10 +55,12 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 			Title        string
 			VerifiedUser bool
 			UserName     string
+			Trending	data.Article
 		}{
 			Title:        "Home",
 			VerifiedUser: false, // Indicates the user is not signed in
 			UserName:     "",
+			Trending: articles,
 		}
 		utils.RenderTemplate(w, "index.html", data)
 		return
@@ -46,10 +81,12 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		Title        string
 		VerifiedUser bool
 		UserName     string
+		Trending 	data.Article
 	}{
 		Title:        "Home",
 		VerifiedUser: signed,
 		UserName:     userName,
+		Trending: articles,
 	}
 
 	utils.RenderTemplate(w, "index.html", data)
