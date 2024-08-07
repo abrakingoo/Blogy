5ef4e09ca386045c3a12eed8aa7ae2da1e2da98d
abrakingoo <maikoabraham68@gmail.com>
2024-08-07 09:14:07 +0300

    feat: added the article struct to represent the article received fro the trending api call

--- data/user.go
+++ data/user.go
@@ -17,3 +17,23 @@ type User struct {
 	Create_at time.Time `json:"created_at"`
 	Blogs     []Blog    `json:"blogs"`
 }
+
+// Source represents the source of an article.
+type Source struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Author      string `json:"author"`
+	Title       string `json:"title"`
+	Description *string `json:"description"` // Nullable field
+	URL         string `json:"url"`
+	Image    *string `json:"image"` // Nullable field
+	PublishedAt string `json:"publishedAt"`
+	Content     *string `json:"content"` // Nullable field
+}
+
+// Article represents a news article with various attributes.
+type Article struct {
+	Status string `json:"status"`
+	Totalresults int `json:"totalresults"`
+	Articles []Source
+}
