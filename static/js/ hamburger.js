ddacebf51ca4a9540c50de1d45cf1d1f49bea847
abrakingoo <maikoabraham68@gmail.com>
2024-08-07 09:26:13 +0300

    script: added scripts for setting the active category abd setting up the quil editor for writting and editing posts

--- static/js/ hamburger.js
+++ static/js/ hamburger.js
@@ -7,34 +7,7 @@ document.querySelector(".fas").addEventListener("click", () => {
   }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Check if the script has run before in this session
-  if (!sessionStorage.getItem("scriptRan")) {
-    // Automatically load the "Trending" category
-    fetchCategory("trending");
-
-    // Mark the script as having run
-    sessionStorage.setItem("scriptRan", "true");
-  }
-});
-
-function fetchCategory(categoryName) {
-  fetch(`/category?category=${categoryName}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((response) => response.text())
-    .then((html) => {
-      // Update the content of the page
-      document.querySelector(".pages").innerHTML = html;
-    })
-    .catch((error) => {
-      console.error("Error fetching category:", error);
-    });
-}
-
+// error message function
 function ShowErrorMessage() {
   let message = document.querySelector(".login-error");
 
@@ -48,3 +21,55 @@ function ShowErrorMessage() {
     }, 3000);
   }
 }
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  let linkContainers = document.querySelectorAll(".categs");
+
+  linkContainers.forEach((container) => {
+    container.addEventListener("click", (event) => {
+      let link = container.querySelector("a");
+      let url = link.getAttribute("href");
+
+      // Remove "active" class from all link containers
+      linkContainers.forEach((container) => {
+        container.classList.remove("active");
+      });
+
+      // Add "active" class to the clicked link container
+      container.classList.add("active");
+      history.pushState(null, "", url);
+    });
+  });
+});
+
+///editor
+document.addEventListener("DOMContentLoaded", function () {
+  var toolbarOptions = [
+    ["bold", "italic", "underline", "strike"],
+    ["blockquote", "code-block"],
+    [{ header: 1 }, { header: 2 }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ script: "sub" }, { script: "super" }],
+    [{ indent: "-1" }, { indent: "+1" }],
+    [{ direction: "rtl" }],
+    [{ size: ["small", false, "large", "huge"] }],
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    [{ color: [] }, { background: [] }],
+    [{ font: ["Arial"] }],
+    [{ align: [] }],
+    ["clean"],
+  ];
+
+  var quill = new Quill("#editor-container", {
+    modules: {
+      toolbar: toolbarOptions,
+    },
+    theme: "snow",
+  });
+
+  // Function to get Quill content and set it to hidden input
+  document.querySelector(".post-form").addEventListener("submit", function () {
+    var hiddenInput = document.getElementById("hidden-input");
+    hiddenInput.value = quill.root.innerHTML;
+  });
+});
